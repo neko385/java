c652b951c97300eea4149202f1d6d6303bdc9e37
neko385 <102905708+neko385@users.noreply.github.com>
2022-08-27 00:11:24 +0900

    Java 4章確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		// 定数の宣言
+		final int LUCKY_NUMBER = 8;
+		// 「String」型の変数「str」の宣言と初期化して出力
+		String str = "〇〇××";
+		System.out.println(str);
+		//変数「str」に再代入して出力
+		str = "××△△";
+		System.out.println(str);
+		// 「int」型で宣言したクラス定数を出力
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
