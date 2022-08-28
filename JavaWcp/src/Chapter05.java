b47e34885f6f6a6b229b35ac4ee2116514c83c0e
neko385 <102905708+neko385@users.noreply.github.com>
2022-08-28 23:29:37 +0900

    Java 5章確認問題

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 987654321098765L;
+		System.out.println(long1);
+		
+		double double1 = 8765432109.9;
+		System.out.println(double1);
+		
+		boolean  boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 9.9f;
+		System.out.println(float1);
+		
+		Integer integer1 = 6;
+		System.out.println(integer1);
+		
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		int[] array1 = { 0, 10, 20, 30, 40 };
+		System.out.println(array1[3]);
+		}
+
+}
