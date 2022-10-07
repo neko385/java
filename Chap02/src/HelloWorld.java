669ea28604e83222d351c0d3d3ebb5e040dc3dff
neko385 <102905708+neko385@users.noreply.github.com>
2022-10-07 21:40:41 +0900

    Java学習

--- Chap02/src/HelloWorld.java
+++ Chap02/src/HelloWorld.java
@@ -1,6 +1,10 @@
-
+/*
+ Javaの基本となるプログラム
+ ここから学習を始める。
+ */
 public class HelloWorld {
 	public static void main(String[] args) {
+		//「Hello World!」と標準出力。
 		System.out.println("Hello World!");
 	}
 }
