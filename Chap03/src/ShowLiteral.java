b0bc3da27b1fca73a4a5c167363f64de79134e9b
neko385 <102905708+neko385@users.noreply.github.com>
2022-10-11 21:42:15 +0900

    Java学習

--- Chap03/src/ShowLiteral.java
+++ Chap03/src/ShowLiteral.java
@@ -1,9 +1,9 @@
 
 public class ShowLiteral {
 	public static void main(String[] args) {
-		System.out.println("123");
-		System.out.println(456);
-		System.out.println(789.1);
+		System.out.println("123"); //文字列型リテラル
+		System.out.println(456);  //整数型リテラル
+		System.out.println(789.1);  //小数型リテラル
 	}
 
 }
---
+++ Chap03/src/UseVariables.java
@@ -0,0 +1,14 @@
+
+public class UseVariables {
+	public static void main(String args[]) {
+		// 変数の型　変数名 
+		String name = "佐藤";  // 文字列型→String
+		int num = 551;        // 整数型→int
+		double dec = 67.8;    // 小数型→double
+		
+		System.out.println(name);
+		System.out.println(num);
+		System.out.println(dec);
+	}
+
+}
