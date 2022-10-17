82fbd5857b0f8cb41d7cd3fc917200330a5fa6e6
neko385 <102905708+neko385@users.noreply.github.com>
2022-10-17 22:08:05 +0900

    Java学習

---
+++ Chap03/src/StringsJoin2.java
@@ -0,0 +1,12 @@
+
+public class StringsJoin2 {
+	public static void main(String[] args) {
+		String str = "変数numの値は";
+		int num = 5573;
+		
+		String joinedStr = str + num;
+		
+		System.out.println(joinedStr);
+	}
+
+}
