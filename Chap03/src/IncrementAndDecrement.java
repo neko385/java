bd39f18a7b2f990cf38e090370ef99b42a70ab66
neko385 <102905708+neko385@users.noreply.github.com>
2022-10-20 23:07:08 +0900

    Java学習

--- Chap03/src/IncrementAndDecrement.java
+++ Chap03/src/IncrementAndDecrement.java
@@ -4,8 +4,11 @@ public class IncrementAndDecrement {
 		int num = 100;
 		num++;
 		System.out.println("numの値: " + num);
+		// 「++」がインクリメント演算子。値が１ずつ増えていく。
 		num--;
 		System.out.println("numの値: " + num);
+		// 「--」がデクリメント演算子。値が１ずつ減っていく。
+		
 	}
 
 }
---
+++ Chap04/src/IfStatement.java
@@ -0,0 +1,16 @@
+
+public class IfStatement {
+	public static void main(String[] args) {
+		int num = 30;
+		/* 
+		 * if(条件){
+		 * 	条件に合致した時に行う処理
+		 * }
+		 */
+		if(num <= 40) {
+			System.out.println("数値は40以下です。");
+		}
+		System.out.println("処理終了！");
+	}
+
+}
